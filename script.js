67c65c6198bfbbfe3689a6487e4ff864023af7da
Djazz22 <106832875+Djazz22@users.noreply.github.com>
2024-07-04 01:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
 
+    // Get the form element
+    const form = document.getElementById('form1');
+
+    // Get the values of first name and last name from the form inputs
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the values using an alert
+    alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
 }
